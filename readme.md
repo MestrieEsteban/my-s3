e658f4838f381a9b762290468f51ef06073fdeba
MestrieEsteban <esteban94.em@gmail.com>
2020-12-04 09:44:32 +0100

    ok

---
+++ readme.md
@@ -0,0 +1,1 @@
+['Collection postman'](https://www.getpostman.com/collections/603a853c16b417ae9a94)
--- src/routes/api/secured/routes.upload.ts
+++ src/routes/api/secured/routes.upload.ts
@@ -3,130 +3,142 @@ import multer from 'multer'
 const path = require('path')
 
 import fs from 'fs'
-import { success } from '@/core/helpers/response'
-import { CREATED } from '@/core/constants/api'
+import { error, success } from '@/core/helpers/response'
+import { BAD_REQUEST, CREATED, OK } from '@/core/constants/api'
 
 const api = Router()
 
 function itExist(dir: string): boolean {
-  if (fs.existsSync(dir)) {
-    return true
-  } else {
-    return false
-  }
+	if (fs.existsSync(dir)) {
+		return true
+	} else {
+		return false
+	}
 }
 const upload = multer({
-  storage: multer.diskStorage({
-    destination: (req, file, callback) => {
-      const { uuid, bucketName } = req.params
-      const path = `./upload/${uuid}/${bucketName}`
-      callback(null, path)
-    },
-    filename: (req, file, callback) => {
-      //originalname is the uploaded file's name with extn
-      callback(null, file.originalname)
-    },
-  }),
+	storage: multer.diskStorage({
+		destination: (req, file, callback) => {
+			const { uuid, bucketName } = req.params
+			const path = `./upload/${uuid}/${bucketName}`
+			callback(null, path)
+		},
+		filename: (req, file, callback) => {
+			//originalname is the uploaded file's name with extn
+			callback(null, file.originalname)
+		},
+	}),
 })
 api.post('/uploadFile/:uuid/:bucketName', upload.single('file'), (req, res) => {
-  try {
-    res.send(req.file)
-  } catch (err) {
-    res.send(err)
-  }
+	try {
+		res.send(req.file)
+	} catch (err) {
+		res.send(err)
+	}
 })
 
 api.post('/createBucket', (req, res) => {
-  const { uuid, bucketName } = req.body
-  try {
-    fs.mkdirSync(`./upload/${uuid}/${bucketName}`)
-    res.send('Bucket created')
-  } catch (err) {
-    res.send(err)
-  }
+	const { uuid, bucketName } = req.body
+	const dir = `./upload/${uuid}/${bucketName}/`
+	if (!itExist(dir)) {
+		fs.mkdirSync(dir)
+		res.status(CREATED.status).json('Bucket created')
+	} else {	
+		res.status(BAD_REQUEST.status).json(error(BAD_REQUEST, new Error('Bucket already existing')))
+	}
 })
 
 api.put('/updateBucket', (req, res) => {
-  const { uuid, bucketName, newBucketName } = req.body
-  try {
-    fs.renameSync(`./upload/${uuid}/${bucketName}`, `./upload/${uuid}/${newBucketName}`)
-    res.send('Bucket Updated')
-  } catch (err) {
-    res.send(err)
-  }
+	const { uuid, bucketName, newBucketName } = req.body
+	try {
+		fs.renameSync(`./upload/${uuid}/${bucketName}`, `./upload/${uuid}/${newBucketName}`)
+		res.send('Bucket Updated')
+	} catch (err) {
+		res.send(err)
+	}
 })
 
 api.delete('/deleteBucket', (req, res) => {
-  const { uuid, bucketName } = req.body
-  try {
-    fs.rmdirSync(`./upload/${uuid}/${bucketName}`, { recursive: true })
-    res.send('Bucket deleted')
-  } catch (err) {
-    res.send(err)
-  }
+	const { uuid, bucketName } = req.body
+	try {
+		fs.rmdirSync(`./upload/${uuid}/${bucketName}`, { recursive: true })
+		res.send('Bucket deleted')
+	} catch (err) {
+		res.send(err)
+	}
 })
 
 api.get('/getBlob', (req, res) => {
-  const { uuid, bucketName } = req.body
-  const dir = `./upload/${uuid}/${bucketName}/`
-  const allFiles: any[][] = []
-  try {
-    const files = fs.readdirSync(dir)
-    files.forEach((file) => {
-      const fileStats = fs.statSync(dir + file)
-      allFiles.push([file, fileStats.birthtime, fileStats.size, path.extname(dir + file).substr(1), dir + file])
-    })
-    res.status(CREATED.status).json(allFiles)
-  } catch (err) {
-    console.log(err)
-  }
+	const { uuid, bucketName } = req.body
+	const dir = `./upload/${uuid}/${bucketName}/`
+	const allFiles: any[][] = []
+	try {
+		const files = fs.readdirSync(dir)
+		files.forEach((file) => {
+			const fileStats = fs.statSync(dir + file)
+			allFiles.push([file, fileStats.birthtime, fileStats.size, path.extname(dir + file).substr(1), dir + file])
+		})
+		res.status(CREATED.status).json(allFiles)
+	} catch (err) {
+		console.log(err)
+	}
 })
 
 api.get('/getBucket', (req, res) => {
-  const { uuid, bucketName } = req.body
-  const dir = `./upload/${uuid}`
-  const allFiles: any[][] = []
-  try {
-    const files = fs.readdirSync(dir)
-    files.forEach((file) => {
-      const fileStats = fs.statSync(dir + file)
-      allFiles.push([file, fileStats.birthtime, fileStats.size, path.extname(dir + file).substr(1), dir + file])
-    })
-    res.status(CREATED.status).json(allFiles)
-  } catch (err) {
-    console.log(err)
-  }
+	const { uuid } = req.body
+	const dir = `./upload/${uuid}`
+	const allFiles: any[][] = []
+	try {
+		const files = fs.readdirSync(dir)
+		files.forEach((file) => {
+			const fileStats = fs.statSync(dir+ '/' + file)
+			allFiles.push([file, fileStats.birthtime, fileStats.size, dir + '/' + file])
+		})
+		res.status(CREATED.status).json(allFiles)
+	} catch (err) {
+		console.log(err)
+	}
 })
 
 api.delete('/deleteFile', (req, res) => {
-  const { uuid, bucketName, fileName } = req.body
-  const dir = `./upload/${uuid}/${bucketName}/${fileName}`
-
-  try {
-    fs.unlinkSync(dir)
-    res.send('file deleted')
-  } catch (err) {
-    res.send(err)
-  }
+	const { uuid, bucketName, fileName } = req.body
+	const dir = `./upload/${uuid}/${bucketName}/${fileName}`
+	try {
+		fs.unlinkSync(dir)
+		res.send('file deleted')
+	} catch (err) {
+		res.send(err)
+	}
 })
 
 api.head('/verifBucket', (req, res) => {
-  const { uuid, bucketName } = req.body
-  const dir = `./upload/${uuid}/${bucketName}/`
-  itExist(dir) ? res.send(200) : res.send(400)
+	const { uuid, bucketName } = req.body
+	const dir = `./upload/${uuid}/${bucketName}/`
+	itExist(dir) ? res.send(200) : res.send(400)
 })
 
 api.copy('/copyBlob', (req, res) => {
-  const { uuid, bucketName, fileName } = req.body
-  const dir = `./upload/${uuid}/${bucketName}/`
-  ///
-  ///
-  ///
+	const { uuid, bucketName, fileName } = req.body
+	const dir = `./upload/${uuid}/${bucketName}/${fileName}`
+	let ext = path.extname(dir).substr(1)
+	let file = fileName.substr(0, fileName.length - ext.length - 1)
+	let copy = `./upload/${uuid}/${bucketName}/${file}.copy.${ext}`
+	try {
+		fs.copyFileSync(dir, copy, fs.constants.COPYFILE_EXCL);
+		res.status(CREATED.status).json('Copy created')
+	} catch (err) {
+		res.send(err)
+	}
+	
+	
 })
 
 api.get('/downloadBlob', (req, res) => {
-  const { uuid, bucketName, fileName } = req.body
-  const dir = `./upload/${uuid}/${bucketName}/${fileName}`
-  res.download(dir)
+	const { uuid, bucketName, fileName } = req.body
+	const dir = `./upload/${uuid}/${bucketName}/${fileName}`
+	if (itExist(dir)) {
+		res.download(dir)
+	} else {
+		res.status(BAD_REQUEST.status).json(error(BAD_REQUEST, new Error('No such file or directory')))
+	}
 })
 export default api
